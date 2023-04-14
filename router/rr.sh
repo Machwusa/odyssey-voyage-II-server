6c390e002d20b510f57bd9c6e38ecb501020fa4e
boitshoko-glucode <boitshoko@glucode.com>
2023-04-14 11:39:38 +0200

    ci

--- router/rr.sh
+++ router/rr.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash\
-\
-APOLLO_KEY=service:My-Graph-j8b549:uWv7DsEWscc4vVEfKrGr_Q APOLLO_GRAPH_REF=My-Graph-j8b549@current ./router --config config.yaml}
+
+APOLLO_KEY=service:My-Graph-j8b549:uWv7DsEWscc4vVEfKrGr_Q APOLLO_GRAPH_REF=My-Graph-j8b549@current ./router --config config.yaml
